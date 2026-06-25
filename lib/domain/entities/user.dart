c856a3f95ced18463e305900b3a296c6013f23e3
Sebastian Vargas Ramirez <zain-wave@outlook.com>
2026-06-25 15:24:46 -0500

    feat(domain): add User entity

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.idNumber,
+    required this.idType,
+    required this.phone,
+    this.phoneExtra,
+    this.location,
+    this.address,
+    this.photoUrl,
+    required this.createdAt,
+  });
+
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String idNumber;
+  final String idType;
+  final int phone;
+  final int? phoneExtra;
+  final String? location;
+  final String? address;
+  final String? photoUrl;
+  final DateTime createdAt;
+
+  String get fullName => '$firstName $lastName';
+
+  @override
+  List<Object?> get props => [
+        id,
+        firstName,
+        lastName,
+        email,
+        idNumber,
+        idType,
+        phone,
+        phoneExtra,
+        location,
+        address,
+        photoUrl,
+        createdAt,
+      ];
+}
